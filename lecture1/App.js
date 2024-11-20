e08bdad946ea72d804100eca0c48d430673d5b96
tonyArya009 <tonyarya009@gmail.com>
2024-11-20 19:27:58 +0530

    Updates App.js and added required dependencies

--- lecture1/App.js
+++ lecture1/App.js
@@ -32,7 +32,7 @@
                 [React.createElement('h1',{},"i am h1 tag"),React.createElement('h2',{},"i am h2 tag")]
             ),
             React.createElement('div',{id:'child2'},
-                [React.createElement('h1',{},"i am h1 tag"),React.createElement('h2',{},"i am h2 tag")])
+                [React.createElement('h1',{},"i am h1 tag but cooler"),React.createElement('h2',{},"i am h2 tag")])
             ]);
         const root=ReactDOM.createRoot(document.getElementById("root"));
         root.render(parent); // putting parent into root tag
